7766b0e9440ba88ff9d3a5ca1871f8943362c6a1
Perfight <m0nter1327@mail.ru>
2023-02-25 12:43:31 +0300

    Activity Main

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,22 +1,50 @@
 package com.example.test;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "StartActivity";
+
+    private static final String KEY_COUNTER = "COUNTER";
     private Integer counter = 0;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState == null){
+            Toast.makeText(this, "Пиветствую, дорогой любитель чая <3", Toast.LENGTH_LONG).show();
+        }
     }
 
-    public void onClickBtnAddTea(View view) {
-        counter++;
-        TextView counterView = (TextView)findViewById(R.id.txt_counter);
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_COUNTER, counter);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        counter = savedInstanceState.getInt(KEY_COUNTER);
+        TextView counterView = findViewById(R.id.txt_counter);
         counterView.setText(counter.toString());
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void OnClickBtnAddTea(View view){
+        counter ++;
+        TextView counterView = findViewById(R.id.txt_counter);
+        counterView.setText(counter.toString() + " Rub");
     }
 }
